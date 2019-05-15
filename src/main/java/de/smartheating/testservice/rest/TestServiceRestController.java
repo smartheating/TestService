eddd331ebd22395a6440bc1ed6d146ed40344d02
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-15 15:25:15 +0200

    Added commons to dependencies, fixed eureka config

--- src/main/java/de/smartheating/testservice/rest/TestServiceRestController.java
+++ src/main/java/de/smartheating/testservice/rest/TestServiceRestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.smartheating.SmartHeatingCommons.TestModel;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
@@ -18,7 +19,9 @@ public class TestServiceRestController {
 	@GetMapping(value = "/test", produces = "application/json")
 	@ApiOperation(value = "Get all Git-Configurations")
 	public ResponseEntity<?> getAllConfigs() {
-		return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+		TestModel test = new TestModel();
+		test.setTest("Test!");
+		return new ResponseEntity<>(test, HttpStatus.OK);
 	}
 
 }
