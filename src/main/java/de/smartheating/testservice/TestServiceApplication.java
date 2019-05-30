0392a2ae4b9744e2d988f7731825a1da9b9793ad
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-30 08:43:57 +0200

    Cleaned up Database exclusion

--- src/main/java/de/smartheating/testservice/TestServiceApplication.java
+++ src/main/java/de/smartheating/testservice/TestServiceApplication.java
@@ -2,9 +2,11 @@ package de.smartheating.testservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @EnableDiscoveryClient
 public class TestServiceApplication {
 
