368960d2345357ad1f2685387a8c101a6586cc10
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-06-11 17:52:15 +0200

    Fixed AWS Configuration

--- src/main/java/de/smartheating/testservice/TestServiceApplication.java
+++ src/main/java/de/smartheating/testservice/TestServiceApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@ComponentScan(basePackages = {"de.smartheating.SmartHeatingCommons","de.smartheating.testservice"})
 @EnableDiscoveryClient
 public class TestServiceApplication {
 
